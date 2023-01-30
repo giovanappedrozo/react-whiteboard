497dc0e5d5de7d1721734100df2a35f69f91305e
giovanappedrozo <giovanapp2003@gmail.com>
2023-01-30 12:16:09 -0300

    feat: first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactFlow, {
+  Background,
+  Controls,
+  Node,
+  ConnectionMode,
+  useEdgesState,
+  useNodesState,
+  Connection,
+  addEdge,
+} from "reactflow";
+import { zinc } from "tailwindcss/colors";
+import "reactflow/dist/style.css";
+import { Square } from "./components/nodes/Square";
+import { useCallback } from "react";
+import { Default } from "./components/edges/Default";
+import * as Toolbar from "@radix-ui/react-toolbar";
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const EDGE_TYPES = {
+  default: Default,
+};
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 800,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[];
 
 function App() {
+  const [edges, setEdges, onEdgesChanges] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChanges] = useNodesState(INITIAL_NODES);
+
+  const onConnect = useCallback(
+    (connection: Connection) => {
+      return setEdges((edges) => addEdge(connection, edges));
+    },
+    [setEdges]
+  );
+
+  function addSquareNode() {
+    setNodes((nodes) => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: "square",
+        position: {
+          x: 600,
+          y: 200,
+        },
+        data: {},
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen">
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        connectionMode={ConnectionMode.Loose}
+        edgeTypes={EDGE_TYPES}
+        edges={edges}
+        onEdgesChange={onEdgesChanges}
+        onConnect={onConnect}
+        onNodesChange={onNodesChanges}
+        defaultEdgeOptions={{ type: "default" }}
+      >
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
+      </ReactFlow>
+      <Toolbar.Root className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
+        <Toolbar.Button
+          className="w-32 h-32 bg-violet-500 rounded transition-transform mt-6 hover:-translate-y-4"
+          onClick={addSquareNode}
+        />
+      </Toolbar.Root>
     </div>
   );
 }
---
+++ src/components/edges/Default.tsx
@@ -0,0 +1,42 @@
+import { EdgeProps, getSmoothStepPath } from "reactflow";
+
+export function Default({
+  id,
+  style,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  targetPosition,
+  markerEnd,
+  data,
+}: EdgeProps) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+  return (
+    <>
+      <path
+        id={id}
+        style={style}
+        className="react-flow__edge-path stroke-2 stroke-zinc-300"
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+      <text>
+        <textPath
+          href={`#${id}`}
+          className="text-xs"
+          startOffset="50%"
+          textAnchor="middle"
+        >
+          {data?.text}
+        </textPath>
+      </text>
+    </>
+  );
+}
